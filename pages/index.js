f41919c4f0c590d0c7585cb84b09e4e0de41fb29
Ltwoz <warrapat.102@gmail.com>
2023-06-17 15:03:31 +0700

    Initial Commit. 🔛🔝

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,169 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import { useLoadScript, GoogleMap, MarkerF } from "@react-google-maps/api";
+import { useEffect, useMemo, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+function getCoordinatesFromMapsUrl(url) {
+    const regex = /@(-?\d+\.\d+),(-?\d+\.\d+)/;
+    const matches = url.match(regex);
+    if (matches && matches.length >= 3) {
+        const lat = parseFloat(matches[1]);
+        const lng = parseFloat(matches[2]);
+        return { lat, lng };
+    }
+    return null;
+}
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+    });
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const [url, setUrl] = useState("");
+    const [coordinate, setCoordinate] = useState({
+        lat: undefined,
+        lng: undefined,
+    });
+    const [center, setCenter] = useState({
+        lat: 18.788,
+        lng: 98.985,
+    });
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    const onUrlSubmit = (e) => {
+        e.preventDefault();
+        const coordinate = getCoordinatesFromMapsUrl(url);
+        if (coordinate) {
+            setCoordinate(coordinate);
+            setCenter(coordinate);
+        } else {
+            console.log("Invalid input.");
+        }
+    };
+
+    const onMapClick = (e) => {
+        e.domEvent.preventDefault();
+
+        const clickedPosition = {
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng(),
+        };
+
+        console.log("coordinate :", clickedPosition);
+
+        if (clickedPosition) {
+            setCoordinate(clickedPosition);
+        }
+    };
+
+    const onClear = (e) => {
+        e.preventDefault();
+
+        setCoordinate({
+            lat: "",
+            lng: "",
+        });
+        setUrl("");
+
+		console.log("Clear coordinate.");
+    };
+
+    if (loadError) {
+        return <div>Map cannot be loaded right now, sorry.</div>;
+    }
+
+    return (
+        <main
+            className={`flex flex-col items-center justify-between max-w-[1000px] mx-auto py-8 md:py-20 px-4 ${inter.className}`}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <div className="flex flex-col w-full md:w-2/3">
+                {/* Url input  */}
+                <div className="block">
+                    <label className="block text-xs md:text-sm font-medium tracking-wide">
+                        Ex.{" "}
+                        <span className="text-blue-700">
+                            https://www.google.com/maps/@18.7685198,98.9727598,15z
+                        </span>
+                    </label>
+                    <div className="flex items-center justify-center gap-x-2 mb-4">
+                        <input
+                            type="text"
+                            placeholder="Google maps url"
+                            value={url}
+                            onChange={(e) => setUrl(e.target.value)}
+                            className="border border-gray-300 rounded-md py-1 px-3 w-full"
+                        />
+                        <button
+                            onClick={onUrlSubmit}
+                            className="border border-gray-300 bg-gray-100 hover:bg-gray-200 rounded-md py-1 px-3"
+                        >
+                            Find
+                        </button>
+                    </div>
+                </div>
+                {/* Lat, Lng input */}
+                <div className="grid grid-cols-2 w-full gap-x-2 mb-4">
+                    <div className="col-span-1">
+                        <label className="block text-xs md:text-sm font-medium tracking-wide">
+                            Latitude
+                        </label>
+                        <input
+                            type="number"
+                            placeholder="Latitude"
+                            value={coordinate.lat}
+                            onChange={(e) =>
+                                setCoordinate((prev) => ({
+                                    ...prev,
+                                    lat: e.target.valueAsNumber,
+                                }))
+                            }
+                            className="w-full border border-gray-300 rounded-md py-1 px-3"
+                        />
+                    </div>
+                    <div className="col-span-1">
+                        <label className="block text-xs md:text-sm font-medium tracking-wide">
+                            Longtitude
+                        </label>
+                        <input
+                            type="number"
+                            placeholder="Longtitude"
+                            value={coordinate.lng}
+                            onChange={(e) =>
+                                setCoordinate((prev) => ({
+                                    ...prev,
+                                    lng: e.target.valueAsNumber,
+                                }))
+                            }
+                            className="w-full border border-gray-300 rounded-md py-1 px-3"
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-row-reverse mb-6">
+                    <button
+                        onClick={onClear}
+                        className="border border-gray-300 bg-gray-100 hover:bg-gray-200 rounded-md py-1 px-3"
+                    >
+                        Refresh
+                    </button>
+                </div>
+            </div>
+            {/* Map Component */}
+            {!isLoaded ? (
+                <h1>Loading...</h1>
+            ) : (
+                <>
+                    <GoogleMap
+                        mapContainerClassName="w-full h-[200px] md:h-[600px]"
+                        center={center}
+                        zoom={14}
+                        onClick={onMapClick}
+                    >
+                        {coordinate.lat && coordinate.lng ? (
+                            <MarkerF position={coordinate} />
+                        ) : null}
+                    </GoogleMap>
+                </>
+            )}
+        </main>
+    );
 }
